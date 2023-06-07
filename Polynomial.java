8293cc1ccd24a651fd0aa2672f43065b3eb8b613
murphylee10 <murphylee2004@gmail.com>
2023-06-07 13:46:03 -0400

    remove unused imports

--- Polynomial.java
+++ Polynomial.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.PrintStream;
-import java.util.Arrays;
 
 public class Polynomial {
     double[] coefficients;
